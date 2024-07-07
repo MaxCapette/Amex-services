32066e16e658cd8c1d73c0720902a4afc6a556fc
Dokuyaku <sk8baz@gmail.com>
2024-07-07 16:11:02 +0200

    add contact form and phone number

--- src/components/component/main-page.tsx
+++ src/components/component/main-page.tsx
@@ -279,7 +279,7 @@ export function MainPage() {
           <p className="   ">13001 MARSEILLE</p>
           <p className="  mt-4 ">contact@amexservices.fr</p>
 
-          <p className="  mt-4 ">TEL: 04 84 89 63 48</p>
+          <p className="  mt-4 ">TEL: 04 84 89 63 50</p>
           </div>
       </footer>
     </div>
